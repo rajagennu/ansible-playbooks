a2f0e56904717c463de444ed81b4146d82b7f98d
Raja Genupula <raja.genupula@yahoo.com>
2021-08-11 17:40:41 +0530

    playbook updated, sshd playbook

---
+++ test_playbook/dump.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test (
+	message varchar(255) NOT NULL
+	) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+INSERT INTO test(message) VALUES('Ansible to do list');
+INSERT INTO test(message) VALUES('Get Ready');
+INSERT INTO test(message) VALUES("Ansible is fun");
